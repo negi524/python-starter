ff914099655c88d4884ab91f1770ec5bdf750f1b
negi524 <egtnrumpbiizarre.info@gmail.com>
2023-03-05 11:50:27 +0900

    create credential setting

--- python_starter/main.py
+++ python_starter/main.py
@@ -1,12 +1,18 @@
 import application_setting.my_logger as mylogger
 from logging import Logger
+import application_setting.my_credential as mycredential
+from typing import Optional
 
 # loggerを取得
 logger: Logger = mylogger.get_logger("main")
 
+# credential情報を取得
+credential: Optional[dict] = mycredential.read_credential()
+
 
 def main():
     logger.info("hello, world.")
+    print(credential)
 
 
 def add_one(number: int) -> int:
