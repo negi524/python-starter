9f483133eeb7cb712d2c0dde79f66d7ebe55d94f
negi524 <egtnrumpbiizarre.info@gmail.com>
2023-03-05 11:51:02 +0900

    create credential setting (#7)

--- python_starter/application_setting/my_credential.py
+++ python_starter/application_setting/my_credential.py
@@ -1,3 +1,16 @@
-def get_credential() -> str:
-    # TODO: 実装
-    return "credenatial"
+import yaml
+from pathlib import Path
+from typing import Optional
+
+credential_file_path: str = (
+    f"{Path(__file__).resolve().parent.parent}/config/credential.yml"
+)
+
+
+def read_credential() -> Optional[dict]:
+    """credential情報を読み込み、辞書型で返却する
+    Returns:
+        dict: credential.ymlに記載された情報
+    """
+    with open(credential_file_path) as file:
+        return yaml.safe_load(file)
